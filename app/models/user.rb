93c788ad868d2394a5268a0886adc69e31824555
Curtis Dunne <curtdunne@gmail.com>
2019-02-14 20:43:55 +0000

    added association for plans to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # associates users with plans
+  belongs_to :plan 
 end
